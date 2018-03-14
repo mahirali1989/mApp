7db18699ba3998a71dba86ef876f5f85bf62a5b3
TA378505 <tanaya.chimnani@wipro.com>
2018-03-14 18:48:35 +0530

    new Functionality

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = true;
+  serverStatus= 'Server is offline';
+  serverInput = 'TestServer';
+  serveCreate= false;
   constructor() { 
     setTimeout( () => {
       this.allowNewServer = false;
     },2000)
   }
-
+  onCreateServer () {
+    this.serveCreate= true;
+    this.serverStatus ='Server is online,'+ 'name of the server is:' +this.serverInput;
+  }
+  onUpdateServer (event: Event) {
+    console.log(event);
+    this.serverInput = (<HTMLInputElement>event.target).value;
+  }
   ngOnInit() {
   }
 
